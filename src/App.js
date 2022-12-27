dae5446096f6dc95e6c785a42e96cac37df67cca
KevinYSM <22753615@student.uwa.edu.au>
2022-12-27 20:46:10 +0800

    add end credits

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Chessboard from "./components/chessboard/chessboard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/navbar/navbar";
+import Endcredits from "./components/endcredits/endcredits"
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
        
         <Navbar/>
       <Chessboard num_rows={8} num_cols={8} />
-      
+      <Endcredits/>
     </div>
   );
 }
--- src/components/endcredits/endcredits.js
+++ src/components/endcredits/endcredits.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./endcredits.css"
+
 
 function endcredits() {
   return (
-    <div>endcredits</div>
+        
+    <div id="end_credits_container">
+        <div id="made_by_me">
+        Made by me using React. © Kevin Shah Mansouri <span id="year">{(new Date().getFullYear())}</span>
+        </div>
+    </div>
   )
 }
+
 
 export default endcredits
