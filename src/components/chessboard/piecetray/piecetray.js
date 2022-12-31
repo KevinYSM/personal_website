ecd1f811f14218984bae0999e04b8e1a0376c81f
KevinYSM <22753615@student.uwa.edu.au>
2022-12-31 13:59:09 +0800

    initial piece tray

---
+++ src/components/chessboard/piecetray/piecetray.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./piecetray.css"
+function piecetray() {
+  return (
+
+    <div id="piecetray_container">
+        <div id="piecetray"></div>
+    </div>
+  )
+}
+
+function addPieces(){
+        let tray=document.getElementById("piecetray");
+}
+
+export default piecetray
