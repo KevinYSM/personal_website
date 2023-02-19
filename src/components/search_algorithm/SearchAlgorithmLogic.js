1482a9a322cbe23069dd607b89bc777a4744f36e
KevinYSM <kevinshahm11@gmail.com>
2023-02-19 12:24:15 +0800

    working search algorithm

--- src/components/search_algorithm/SearchAlgorithmLogic.js
+++ src/components/search_algorithm/SearchAlgorithmLogic.js
@@ -1,28 +1,61 @@
 export function getShortestPath(mapState){
     let numCols=mapState.length;
     let numRows=mapState[1].length;
-    let vistedNodes = Array(numCols).fill().map(() => Array(numRows).fill(-1));
- 
+    let visitedNodes = Array(numCols).fill().map(() => Array(numRows).fill(-1));
+    let sparseVisitedNodes=Array(numCols).fill().map(() => Array(numRows).fill(-1));
     let startingPos=getStartingSquare(mapState);
-
+    console.log(visitedNodes)
     let reachedEnd=false;
     let nodeQueue=[startingPos]
-    vistedNodes[startingPos[0]][startingPos[1]]=0;
-    while (reachedEnd!=true | nodeQueue!=[]){
-        for (let i=0; i<nodeQueue.length;i++){
-            let node=nodeQueue[i];
-            if (mapState[node[0]][node[1]]===3){
-                reachedEnd=true;
-            }
-            else{
-                nodeQueue.concat(getAdjacentNodes(mapState, vistedNodes, node));
-            }
-            vistedNodes[node[0]][node[1]]
+    visitedNodes[startingPos[0]][startingPos[1]]=[0,[startingPos[0]][startingPos[1]]];
+    sparseVisitedNodes[startingPos[0]][startingPos[1]]=0;
+    let transposed_output = sparseVisitedNodes[0].map((_, colIndex) => sparseVisitedNodes.map(row => row[colIndex]));
+    let finalPath=[];
+   
+    while (reachedEnd!=true){
+        if (nodeQueue.length==0){
+            break;
         }
+        let currentNode=nodeQueue.shift();
+
+        if (mapState[currentNode[0]][currentNode[1]]===3){
+            reachedEnd=true;
+            finalPath.unshift(currentNode);
+            console.log(currentNode)
+            let pathCompleted=false;
+            let previousNode;
+            while (pathCompleted==false){
+                previousNode=visitedNodes[currentNode[0]][currentNode[1]][1]
+                finalPath.unshift(previousNode);
+                currentNode=previousNode;
+                if (previousNode==startingPos){
+                    pathCompleted=true;
+                }
+            }
+        }
+        else{
+    
+            let adjacentNodes=getAdjacentNodes(mapState, visitedNodes, currentNode, sparseVisitedNodes);
+        
+            for (let j=0;j<adjacentNodes.length;j++){
+                let potentialNextNode=adjacentNodes[j];
+                if (visitedNodes[potentialNextNode[0]][potentialNextNode[1]]===-1) //If node is unvisited, add to nodeQueue
+                {
+                    let updatedVisitedNode=[false,false]
+                    updatedVisitedNode[0]=visitedNodes[currentNode[0]][currentNode[1]][0]+1;
+                    updatedVisitedNode[1]=currentNode;
+                    sparseVisitedNodes[potentialNextNode[0]][potentialNextNode[1]]=visitedNodes[potentialNextNode[0]][potentialNextNode[1]][0];
+                    visitedNodes[potentialNextNode[0]][potentialNextNode[1]]=updatedVisitedNode;
+                    nodeQueue.push(potentialNextNode);
+                }
+            }
+            
+        }
+
     }
-    console.log(vistedNodes)
-
-
+    console.log(visitedNodes)
+    console.log(finalPath)
+    highlightFinalPath(finalPath)
 
 }
 
@@ -41,22 +74,45 @@ function getStartingSquare(mapState){
     }
 }
 
-function getAdjacentNodes(mapState, visitedNodes, currentSquare){
+function highlightFinalPath(finalPath){
+    for (let i=0; i<finalPath.length; i++){
+        let currentSquare=getSquare(finalPath[i]);
+        currentSquare.style.backgroundColor="red";
+
+    }
+}
+
+function getSquare(coordinates) //use cookies for this
+{
+    let numRows=10;
+    let numCols=20;
+    let x_coordinate=coordinates[0];
+    let y_coordinate=coordinates[1];
+
+    return (document.getElementById(x_coordinate+(numRows-y_coordinate-1)*numCols))
+}
+function getAdjacentNodes(mapState, visitedNodes, currentSquare, sparseVisitedNodes){
     let adjacentNodes=[]
     let numCols=mapState.length;
     let numRows=mapState[1].length;
     let pos_change=[[-1,0],[1,0],[0,-1],[0,1]]
     let nextSquare;
-
+  
     for (let i=0;i<pos_change.length;i++){
+        
         nextSquare=[ (currentSquare[0]+pos_change[i][0]) , (currentSquare[1]+pos_change[i][1]) ]
-        if (nextSquare[0]<numRows && nextSquare[0]>=0){
-            if (nextSquare[1]<numCols && nextSquare[1]>=0){
-                if (mapState[nextSquare[0]][nextSquare[1]]!=2){
-                    if (visitedNodes[nextSquare[0]][nextSquare[1]]===-1){
+        if (nextSquare[0]<numCols && nextSquare[0]>=0) //within bounds
+        {
+            if (nextSquare[1]<numRows && nextSquare[1]>=0) //within bounds
+            {
+                
+                if (mapState[nextSquare[0]][nextSquare[1]]!=2) //not a wall
+                {
+                    if (visitedNodes[nextSquare[0]][nextSquare[1]]===-1) //not visited
+                    {
+                       
                         adjacentNodes.push(nextSquare)
-                    }
-                    
+                    } 
                 }
                 
             }
